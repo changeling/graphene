1b3b5487461a9abee3e0c06ec53dd6b198e73822
changeling <cklarson@gmail.com>
2019-06-10 23:05:39 -0500

    Set version display for theme.

--- docs/conf.py
+++ docs/conf.py
@@ -149,6 +149,24 @@ todo_include_todos = True
 #
 # html_theme_options = {}
 
+html_theme_options = {
+    # 'canonical_url': '',
+    # 'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
+    # 'logo_only': False,
+    'display_version': True,
+    # 'prev_next_buttons_location': 'bottom',
+    # 'style_external_links': False,
+    # 'vcs_pageview_mode': '',
+    # 'style_nav_header_background': 'white',
+    # Toc options
+    # 'collapse_navigation': True,
+    # 'sticky_navigation': True,
+    # 'navigation_depth': 4,
+    # 'includehidden': True,
+    # 'titles_only': False
+}
+
+
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
 
